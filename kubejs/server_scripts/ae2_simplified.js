98077d825545975a043ed559dfebf94737b281ec
cosmo <71676396+cosmxnautic@users.noreply.github.com>
2026-08-26 22:19:03 -0400

    Server Optimization
    changes to badmobs, disabling tortoise from no mans land as well as festive creepers (AKA reapers) from adapted mobs. added maddie's ae2 recipe and tag changes as well

--- kubejs/server_scripts/ae2_simplified.js
+++ kubejs/server_scripts/ae2_simplified.js
@@ -51,6 +51,9 @@ ServerEvents.recipes(event => {
         //Energy Card
         event.remove({ output: 'ae2:energy_card' })
         event.stonecutting('ae2:energy_card', '#forge:ae_card')
+        //Magnet Card
+        event.remove({ output: 'ae2wtlib:magnet_card' })
+        event.stonecutting('ae2wtlib:magnet_card', '#forge:ae_card')
 
     //Network Blocks
         //Controller
@@ -198,12 +201,23 @@ ServerEvents.recipes(event => {
         //P2P
         event.remove({ output: 'ae2:me_p2p_tunnel' })
         event.stonecutting('2x ae2:me_p2p_tunnel', 'ae2:interface')
+        //Wireless Terminal
+        event.remove({ output: 'ae2wtlib:wireless_universal_terminal' })
+        event.shapeless(
+            Item.of('ae2wtlib:wireless_universal_terminal'),[
+                        'ae2:dense_energy_cell',
+                        'ae2:crafting_terminal',
+                        'aeronautics:levitite_blend_bucket'
+                    ])
         //Wireless Access
         event.remove({ output: 'ae2:wireless_access_point' })
         event.stonecutting('ae2:wireless_access_point', 'create:brass_casing')
         //Wireless Booster
         event.remove({ output: 'ae2:wireless_booster' })
         event.stonecutting('16x ae2:wireless_booster', 'botania:corporea_spark')
+        //Quantum Booster
+        event.remove({ output: 'ae2wtlib:quantum_bridge_card' })
+        event.stonecutting('ae2wtlib:quantum_bridge_card', 'ae2:singularity')
     
     //Tools
         //Entropy
@@ -236,8 +250,6 @@ ServerEvents.recipes(event => {
         event.stonecutting('ae2:meteorite_compass', '#forge:ae_tools')
         
     //Network
-        //Quartz Fiber
-        event.stonecutting('ae2:quartz_fiber', '#c:gems/quartz')
         //Fluix Cable
         event.remove({ output: 'ae2:fluix_glass_cable' })
         event.shapeless(
